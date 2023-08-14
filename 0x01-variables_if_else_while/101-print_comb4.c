e68b104b98004bb1cf394cead3fc231e981af94a
Karthy <kathyandrea60@gmail.com>
2023-08-14 07:46:51 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,12 +16,12 @@ int main(void)
 		{
 			for (l = 50; l < 58; l++)
 			{
-				if ( l > m && m > n)
+				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
 					putchar(l);	
-					if (n !=55 || m != 56)
+					if (n != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
