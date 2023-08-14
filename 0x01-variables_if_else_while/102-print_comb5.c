89114145cd00cae78709f8cd3b2adf468888888f
Karthy <kathyandrea60@gmail.com>
2023-08-14 03:55:38 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,10 +16,10 @@ int main(void)
 		{
 			if (i < j)
 			{
-				putchar((i /10) + 48);
+				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar((j /10) + 48);
+				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
 				{
