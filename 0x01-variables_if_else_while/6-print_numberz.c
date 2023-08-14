11458e5f39e1f085ffd7885226ed7fdc43820e30
Karthy <kathyandrea60@gmail.com>
2023-08-14 02:14:32 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdii.h>
+
+/**
+ * main - Prints numbers from 0 to 9
+ *
+ * Returns: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
