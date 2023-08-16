0216d4db85e5e0947efc0ce3424df5e1729b3609
Karthy <kathyandrea60@gmail.com>
2023-08-16 14:56:55 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	putchar(' ');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('r');
-	putchar('\n');
+	_putchar('_ ');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
 
