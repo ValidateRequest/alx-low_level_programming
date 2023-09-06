956756415dd8f53caca18b04f0c13afa0a4da1a5
Karthy <kathyandrea60@gmail.com>
2023-09-05 18:58:08 -0700

    101-strtow.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,7 @@
 /**
  * str_concat - get ebds of input and add togrther for size
  * @s1: input one to concat
- * s2: input two to concat
+ * @s2: input two to concat
  * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
