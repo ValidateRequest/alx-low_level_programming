806346295e22c6b8f70dc8a5228cbe1d879063b1
Karthy <kathyandrea60@gmail.com>
2023-09-06 17:45:44 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+        int m, n, o, len, p, digit;
+
+        m = 0;
+        n = 0;
+        o = 0;
+        len = 0;
+        p = 0;
+        digit = 0;
+
+        while (s[len] != '\0')                                                  len++;
+                                                                        while (m < len && p == 0)
+        {
+                if (s[m] == '-')
+                        ++n;
+
+                if (s[m] >= '0' && s[m] <= '9')                                 {
+                        digit = s[m] - '0';
+
+		}
+	}
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - a function that copies the sting pointer
+ * @dest: copy to
+ * @src: cooy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+        int k = 0;
+        int t = 0;
+
+        while (*(src + k) != '\0')
+        {
+                k++;
+        }
+        for ( ; t < k ; t++)
+        {
+                dest[t] = src[t];                                       }
+        dest[k] = '\0';                                                 return (dest);
+}
+
